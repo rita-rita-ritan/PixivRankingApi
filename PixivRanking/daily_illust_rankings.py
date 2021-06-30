2aeb2dc76cacde0139d9c719a35ea5332afabac3
ritan <38023004+rita-rita-ritan@users.noreply.github.com>
2021-06-30 15:50:10 +0900

    implement quiz
    とりあえずそれなりに動くようにはなった

--- PixivRanking/daily_illust_rankings.py
+++ PixivRanking/daily_illust_rankings.py
@@ -10,8 +10,8 @@ class DailyIllustRankings:
     timestamp = ""
 
     def __init__(self):
-        for id in range(1, self.len_works + 1):
-            self.works[id] = {}
+        for rank in range(1, self.len_works + 1):
+            self.works[rank] = {"rank": rank}
 
     def get_now_time(self):
         JST = datetime.timezone(datetime.timedelta(hours=9))  # 日本標準時 (Japan Standard Time)
@@ -22,32 +22,49 @@ class DailyIllustRankings:
         source_html = requests.get('https://www.pixiv.net/ranking.php?mode=daily&content=illust')
         self.get_now_time()
         soup = BeautifulSoup(source_html.text, "html.parser")
-        for i, work_html in enumerate(soup.select(".work")):
-            self.works[i + 1]["work_url"] = "https://www.pixiv.net" + work_html["href"]
 
-    def get_opengraph(self, id, work_url):
-        # id (int): Ranking in Daily Illust Rankings. 1-indexed
+        creator_names = soup.select(".user-name")
+        creator_urls = soup.select(".user-container")
+        work_titles = soup.select(".title")
+        work_urls = ["https://www.pixiv.net" + work_html["href"] 
+                        for work_html in soup.select(".work")]
+
+        for i in range(self.len_works):
+            self.works[i + 1]["creator_name"] = creator_names[i].get_text()
+            self.works[i + 1]["creator_url"] = "https://www.pixiv.net" + creator_urls[i]["href"]
+            self.works[i + 1]["work_title"] = work_titles[i].get_text()
+            self.works[i + 1]["work_url"] = work_urls[i]
+
+    def get_work_opengraph(self, rank, work_url):
+        # rank:int (1-indexed)
 
         source_html = requests.get(work_url)
         soup = BeautifulSoup(source_html.text, "html.parser")
 
         # head > meta property="og:image"
         og_image_url = soup.select_one('meta[property="og:image"]')["content"]
+        self.works[rank]["opengraph_work_image_url"] = og_image_url
 
-        # head > meta property="og:title"
-        og_title = [v.strip() for v 
-            in soup.select_one('meta[property="og:title"]')["content"].split('-')]
-        og_image_title = ''.join(og_title[0].split()[1:])
-        og_creator_name = og_title[-2][:-5]
+    def get_creator_opengraph(self, rank, work_url):
+        # rank:int (1-indexed)
 
-        self.works[id]["og_image_url"] = og_image_url
-        self.works[id]["og_image_title"] = og_image_title
-        self.works[id]["og_creator_name"] = og_creator_name
+        source_html = requests.get(work_url)
+        soup = BeautifulSoup(source_html.text, "html.parser")
+
+        # head > meta property="og:image"
+        og_image_url = soup.select_one('meta[property="og:image"]')["content"]
+        self.works[rank]["opengraph_creator_image_url"] = og_image_url
+
+        # head > meta property="og:description"
+        creator_description = soup.select_one('meta[property="og:description"]')["content"]
+        self.works[rank]["opengraph_creator_description"] = creator_description
 
     def get_opengraphs(self):
-        for id in self.works:
-            time.sleep(10)
-            self.get_opengraph(id, self.works[id]["work_url"])
+        for rank in self.works:
+            time.sleep(2)
+            self.get_work_opengraph(rank, self.works[rank]["work_url"])
+            time.sleep(2)
+            self.get_creator_opengraph(rank, self.works[rank]["creator_url"])
 
     def save_as_json(self):
         with open("data/daily_illust_rankings.json", "w") as f:
