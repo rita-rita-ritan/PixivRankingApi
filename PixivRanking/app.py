a81e51cdc8b07953f117fb24e3e4980247243bed
ritan <38023004+rita-rita-ritan@users.noreply.github.com>
2021-07-11 00:32:16 +0900

    CORS

--- PixivRanking/app.py
+++ PixivRanking/app.py
@@ -1,8 +1,17 @@
 import falcon
 from .daily_illust_rankings_interface import DailyIllustRankingsInterface
 
-app = application = falcon.App()
+class CORSMiddleware:
+    def process_request(self, req, resp):
+        resp.set_header('Access-Control-Allow-Origin', 'https://www.pixivquiz.net')
+
+app = application = falcon.App(middleware=[CORSMiddleware()])
 # note: Gunicorn, by default, expects this to be called "application"
+
+# app = application = falcon.App(middleware=falcon.CORSMiddleware(
+#     allow_origins='https://www.pixivquiz.net', allow_credentials='*'))
+# これだとうまくいかなかった。cf: https://falcon.readthedocs.io/en/3.0.1/api/cors.html
+
 
 image = DailyIllustRankingsInterface()
 app.add_route("/image", image)
